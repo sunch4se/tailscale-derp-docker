8aa727d6f87186f8bd6c6843d4257914ccc129ec
sunch4se <45451054+sunch4se@users.noreply.github.com>
2025-01-26 11:46:59 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env sh
 
-#Start tailscaled and connect to tailnet
+# Start tailscaled and connect to tailnet
 /usr/sbin/tailscaled --state=/var/lib/tailscale/tailscaled.state >> /dev/stdout &
-/usr/bin/tailscale up --accept-routes=true --accept-dns=true --auth-key $TAILSCALE_AUTH_KEY >> /dev/stdout &
 
-#Check for and or create certs directory
+# Build the tailscale up command with conditional flags
+TAILSCALE_UP_CMD="/usr/bin/tailscale up --accept-routes=true --accept-dns=true --auth-key $TAILSCALE_AUTH_KEY"
+
+if [ -n "$ADVERTISE_ROUTES" ]; then
+    TAILSCALE_UP_CMD="$TAILSCALE_UP_CMD --advertise-routes=$ADVERTISE_ROUTES"
+fi
+
+if [ "$ADVERTISE_EXIT_NODE" = "true" ]; then
+    TAILSCALE_UP_CMD="$TAILSCALE_UP_CMD --advertise-exit-node"
+fi
+
+# Run the constructed command
+$TAILSCALE_UP_CMD >> /dev/stdout &
+
+# Check for and/or create certs directory
 mkdir -p /root/derper/$TAILSCALE_DERP_HOSTNAME
 
-#Start Tailscale derp server
-/root/go/bin/derper --hostname $TAILSCALE_DERP_HOSTNAME --bootstrap-dns-names $TAILSCALE_DERP_HOSTNAME -certmode $TAILSCALE_DERP_CERTMODE -certdir /root/derper/$TAILSCALE_DERP_HOSTNAME --stun --verify-clients=$TAILSCALE_DERP_VERIFY_CLIENTS
+# Start Tailscale derp server
+/root/go/bin/derper \
+    --hostname $TAILSCALE_DERP_HOSTNAME \
+    --bootstrap-dns-names $TAILSCALE_DERP_HOSTNAME \
+    --certmode $TAILSCALE_DERP_CERTMODE \
+    --certdir /root/derper/$TAILSCALE_DERP_HOSTNAME \
+    --stun \
+    --verify-clients=$TAILSCALE_DERP_VERIFY_CLIENTS
